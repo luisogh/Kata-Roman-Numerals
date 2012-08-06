9291594701575b9789dae102084378cc2471ccbf
luisogh <luisogh@gmail.com>
2012-08-05 22:14:45 -0500

    mi doceavo commit - numero romano XII

--- features/step_definitions/roman_steps.rb
+++ features/step_definitions/roman_steps.rb
@@ -25,25 +25,26 @@ class Conversor
   			 "I"
   		when 2 then
   			 "II"
-  	    when 3 then
+  	  when 3 then
   			 "III"
   		when 4 then
   			 "IV"
-  	    when 5 then
+  	  when 5 then
   			 "V"
   		when 6 then
   			 "VI"
-  	    when 7 then
+  	  when 7 then
   			 "VII"
-  	    when 8 then
+  	  when 8 then
   			 "VIII"
-  	    when 9 then
+  	  when 9 then
   			 "IX"
-        when 10 then
+      when 10 then
          "X"
-         
+      when 11 then
+         "XI"
   		else
-  			 "XI"
+  			 "XII"
 		end
 	
 	end
